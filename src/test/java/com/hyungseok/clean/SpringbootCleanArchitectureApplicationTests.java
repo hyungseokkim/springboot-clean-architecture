386d3fa9a5bba487b2c525ff5d9d31845ed2ca11
hyungseokkim <khkhvv@gmail.com>
2025-06-24 16:56:46 +0900

    Apply proper .gitignore for Spring Boot project

---
+++ src/test/java/com/hyungseok/clean/SpringbootCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyungseok.clean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCleanArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
